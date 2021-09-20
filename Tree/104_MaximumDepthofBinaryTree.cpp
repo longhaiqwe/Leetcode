03f767458d96d08451472ca9ac27d93b01750f54
longhai <longhaiqwe@126.com>
2021-09-20 23:20:49 +0800

    104. 二叉树的最大深度 - 使用递归

--- Tree/104_MaximumDepthofBinaryTree.cpp
+++ Tree/104_MaximumDepthofBinaryTree.cpp
@@ -1,40 +1,58 @@
 #include "Tree.h"
 #include <queue>
 
-class Solution {
+class Solution
+{
 public:
-  int maxDepth(TreeNode *root) {
-    if (root == nullptr)
-      return 0;
+    int maxDepthIteration(TreeNode *root)
+    {
+        if (root == nullptr)
+            return 0;
 
-    queue<TreeNode *> que;
-    que.push(root);
+        queue<TreeNode *> que;
+        que.push(root);
 
-    int result = 0;
-    while (!que.empty()) {
-      int size = que.size();
-      result++;
+        int result = 0;
+        while (!que.empty())
+        {
+            int size = que.size();
+            result++;
 
-      for (int i = 0; i < size; i++) {
-        TreeNode *cur = que.front();
-        que.pop();
+            for (int i = 0; i < size; i++)
+            {
+                TreeNode *cur = que.front();
+                que.pop();
 
-        if (cur->left)
-          que.push(cur->left);
-        if (cur->right)
-          que.push(cur->right);
-      }
+                if (cur->left)
+                    que.push(cur->left);
+                if (cur->right)
+                    que.push(cur->right);
+            }
+        }
+        return result;
     }
-    return result;
-  }
+
+    int maxDepthRecursion(TreeNode *root)
+    {
+        if (root == nullptr)
+        {
+            return 0;
+        }
+
+        return 1 +
+               max(maxDepthIteration(root->left), maxDepthIteration(root->right));
+    }
 };
 
-int main() {
-  vector<int> data = {1, 2, 3, null, 4, 5, 6, 7, null};
-  TreeNode *root = NULL;
-  root = CreateTree(data);
+int main()
+{
+    vector<int> data = {1, 2, 3, null, 4, 5, 6, 7, null};
+    TreeNode *root = NULL;
+    root = CreateTree(data);
 
-  Solution s;
-  int maxDepth = s.maxDepth(root);
-  cout << maxDepth << " " << endl;
+    Solution s;
+    int maxDepth = s.maxDepthIteration(root);
+    cout << maxDepth << " " << endl;
+    maxDepth = s.maxDepthRecursion(root);
+    cout << maxDepth << " " << endl;
 }
